bdc687d177922a55bffe6c109e80033406bbeef8
EthanZmr <289956508@qq.com>
2022-08-17 18:34:36 +0800

    leetCode66 solution

--- src/main/java/leetcode/LeetCode66_PlusOne.java
+++ src/main/java/leetcode/LeetCode66_PlusOne.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * 给定一个由整数组成的非空数组所表示的非负整数
  * 在该数的基础上加1
@@ -12,11 +14,56 @@ package leetcode;
  */
 public class LeetCode66_PlusOne {
     public static void main(String[] args) {
-
+        int[] digits = {9};
+        int[] arr = plusOne(digits);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static int[] plusOne(int[] digits) {
-
+        if (digits == null || digits.length == 0) {
+            return new int[]{1};
+        }
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                if (digits[i] < 9) {
+                    digits[i] = digits[i] + 1;
+                    return digits;
+                } else {
+                    digits[i] = 0;
+                }
+            } else {
+                if (digits[i] < 9) {
+                    digits[i] = digits[i] + 1;
+                    return digits;
+                } else {
+                    digits[i] = 0;
+                    int[] arr = new int[digits.length+1];
+                    arr[0] = 1;
+                    System.arraycopy(digits, 0, arr, 1, digits.length);
+                    return arr;
+                }
+            }
+        }
         return null;
+    }
+
+    /**
+     * 更简洁的写法
+     * */
+    public static int[] BestWayPlusOne(int[] digits) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{1};
+        }
+
+        for (i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] arr = new int[digits.length + 1];
+        arr[0] = 1;
+        return arr;
     }
 }
--- src/main/java/noob_00/ReverseLinkList.java
+++ src/main/java/noob_00/ReverseLinkList.java
@@ -4,22 +4,139 @@ package noob_00;
  * 反转链表
  */
 public class ReverseLinkList {
-    static class Node {
-        Node prev;
-        Node next;
+    /**
+     * 单链表节点
+     * */
+    static class SingleNode {
+        SingleNode next;
         int data;
+
+        public SingleNode(int val) {
+            this.data = val;
+        }
+    }
+
+    /**
+     * 双链表节点
+     * */
+    static class DoubleNode {
+        DoubleNode prev;
+        DoubleNode next;
+        int data;
+
+        public DoubleNode(int val) {
+            this.data = val;
+        }
     }
 
     public static void main(String[] args) {
-
+        SingleNode node01 = new SingleNode(1);
+        node01.next = new SingleNode(2);
+        node01.next.next = new SingleNode(3);
+        node01 = reverseSingleLinkedList(node01);
+        while (node01 != null) {
+            System.out.print(node01.data + ",");
+            node01 = node01.next;
+        }
+        System.out.println();
+        System.out.println("====================");
+        
+        DoubleNode node001 = new DoubleNode(1);
+        DoubleNode node002 = new DoubleNode(2);
+        DoubleNode node003 = new DoubleNode(3);
+        node001.next = node002;
+        node002.prev = node001;
+        node002.next = node003;
+        node003.prev = node002;
+        node001 = reverseDoubleLinkedList(node001);
+        while(node001 != null) {
+            System.out.print(node001.data + ",");
+            node001 = node001.next;
+        }
+        System.out.println();
     }
 
     // 1. 反转单链表
-    // 2. 反转双链表
     // 3. 用单链表实现队列和栈
     // 4. 双链表实现双端队列
-    public static Node reverseLinkedList(Node head) {
+    public static SingleNode reverseSingleLinkedList(SingleNode head) {
+        SingleNode prev = null;
+        SingleNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
 
-        return new Node();
+        return prev;
     }
+
+
+    // 2. 反转双链表
+    public static DoubleNode reverseDoubleLinkedList(DoubleNode head) {
+        DoubleNode prev = null;
+        DoubleNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            head.prev = next;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 单链表实现队列
+     * */
+    public static class implQueueByLinkedList {
+        private SingleNode head;
+        private SingleNode tail;
+        private int size;
+
+        public int size() {
+            return this.size;
+        }
+
+        public boolean queue(SingleNode node) {
+            
+        }
+
+        public SingleNode dequeue() {
+                
+        }
+        
+    }
+
+    /**
+     * 单链表实现栈
+     * */
+    public static class implStackByLinkedList {
+        private SingleNode head;
+        private int size;
+
+
+        public int size() {
+            return this.size;
+        }
+
+        public boolean push(SingleNode node) {
+
+        }
+
+        public SingleNode pop() {
+            
+        }
+    }
+
+    /**
+     * 双向链表实现双端队列
+     * */
+    public static class implDoubleQueueByDoubleLinkedList {
+        private DoubleNode head;
+        private DoubleNode tail;
+        private int size;
+    }
+    
 }
