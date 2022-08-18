f919ccfd90223fe935264d612d4d655b7fca8948
EthanZmr <289956508@qq.com>
2022-08-18 21:12:45 +0800

    leetCode349 solution

--- src/main/java/leetcode/LeetCode349_IntersectionOfTwoArrays.java
+++ src/main/java/leetcode/LeetCode349_IntersectionOfTwoArrays.java
@@ -22,7 +22,29 @@ public class LeetCode349_IntersectionOfTwoArrays {
     }
 
     public static int[] intersection(int[] nums1, int[] nums2) {
-        
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        // 两个set集合
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+
+        for (int j = 0; j < nums2.length; j++) {
+            if (set1.contains(nums2[j])) {
+                set2.add(nums2[j]);
+            }
+        }
+
+        int[] arr = new int[set2.size()];
+        int k = 0;
+        for (Integer val : set2) {
+            arr[k++] = val;
+        }
+        return arr;
+
     }
 
 }
