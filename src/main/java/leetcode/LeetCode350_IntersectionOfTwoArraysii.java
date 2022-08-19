001d7c7c55d8e79e55b4d665aaa692418b051c5d
EthanZmr <289956508@qq.com>
2022-08-19 17:57:26 +0800

    leetCode350

---
+++ src/main/java/leetcode/LeetCode350_IntersectionOfTwoArraysii.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 给你两个整数数组nums1和nums2, 以数组形式返回两数组的交集
+ * 返回结果中每个元素出现的次数，应与元素在两数组中都出现的次数一致
+ * 如果出现次数不一致，则考虑取较小值。
+ * 可以不考虑输出结果的顺序
+ *
+ * [1, 2, 2, 1]  [2, 2]
+ * return [2, 2]
+ */
+public class LeetCode350_IntersectionOfTwoArraysii {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[] intersect(int[] nums1, int nums2) {
+        
+    }
+}
